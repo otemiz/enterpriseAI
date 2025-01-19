ba7b0108d1a0fea3e041d8ab83fd0ad50c6c66f2
otemiz <ozan.temiz.93@gmail.com>
2025-01-19 10:04:50 +0100

    run the enterpreise AI

---
+++ rag.py
@@ -0,0 +1,48 @@
+import dspy
+import ujson
+import os
+
+
+class RAG(dspy.Module):
+    def __init__(self):
+        self.respond = dspy.ChainOfThought('context, question -> response')
+        self.lm = dspy.LM('openai/gpt-4o-mini')
+        self.embedder = dspy.Embedder('openai/text-embedding-3-small', dimensions=512)
+        self.max_characters = 6000
+        self.topk_docs_to_retrieve = 5
+        self.file_path = "dataset/jira_dataset/deneme.jsonl"
+        self.initEmbedding()
+        dspy.configure(lm=self.lm)
+
+    def updateEmbedding(self, corpus):
+        self.search_object = dspy.retrievers.Embeddings(embedder=self.embedder, corpus=corpus, k=self.topk_docs_to_retrieve)
+
+    def initEmbedding(self):
+        self.last_modified = os.path.getmtime(self.file_path)
+        with open(self.file_path) as f:
+            corpus = [ujson.loads(line)['text'][:self.max_characters] for line in f]
+            print(f"Loaded {len(corpus)} initial documents. Will encode them below.")
+            self.updateEmbedding(corpus)
+
+    def detectFileChanges(self):
+        current_modified = os.path.getmtime(self.file_path)
+        if current_modified != self.last_modified:
+            print("Database has changed!")
+            self.last_modified = current_modified
+            return True
+        return False
+
+    def runEmbedding(self):
+        file_change = self.detectFileChanges()
+        if (file_change):
+            with open(self.file_path) as f:
+                corpus = [ujson.loads(line)['text'][:self.max_characters] for line in f]
+                print(f"Loaded {len(corpus)} updated documents. Will encode them below.")
+                self.updateEmbedding(corpus)
+
+    def forward(self, question):
+        self.runEmbedding()
+        context = self.search_object(question).passages
+        result =  self.respond(context=context, question=question)
+        return result["rationale"], result["response"]
+    
